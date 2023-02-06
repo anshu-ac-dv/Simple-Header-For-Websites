de72b0699b14207a62f3a80332b398204cca4db2
Anshu Kumar <111151635+anshukr963@users.noreply.github.com>
2023-02-06 10:16:22 +0530

    Add files via upload

---
+++ Header2.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <div class="container-fluid fiexd-top shadow bg-white mt-0">
+        <ul class="nav s justify-content-center navbar-lower text-white">
+            <li class="nav-item">
+              <a class="nav-link" href="Html.php">HTML</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CSS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PHP</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Java</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Python</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">C</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">C++</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">JS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">MySQL</a>
+            </li> 
+        </ul>
+    </div>
+  </body>
+</html>
